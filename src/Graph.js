593ef3a0fad07c0e6ba86323b680e9064ad9ea2d
Typhub7 <drevon.caroline@gmail.com>
2024-06-26 08:40:16 +0200

    3D metal effect adding

--- src/Graph.js
+++ src/Graph.js
@@ -25,9 +25,25 @@ const Graph = () => {
 
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.enableDamping = true;
-    controls.dampingFactor = 0.25;
+    controls.dampingFactor = 0.5;
     controls.screenSpacePanning = false;
     controls.maxPolarAngle = Math.PI / 2;
+
+    // Ajout des lumières
+    const ambientLight = new THREE.AmbientLight(0xffffff, 50000); // Lumière ambiante faible
+    scene.add(ambientLight);
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 50000);
+    directionalLight.position.set(0.5, 0.5, 0.5);
+    scene.add(directionalLight);
+
+    const pointLight = new THREE.PointLight(0xffffff, 50000);
+    pointLight.position.set(-1, -1, -1);
+    scene.add(pointLight);
+
+    const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 50000);
+    hemiLight.position.set(0, 1, 0);
+    scene.add(hemiLight);
 
     const elements = transformData(termsData);
 
@@ -43,10 +59,34 @@ const Graph = () => {
     const loader = new FontLoader();
 
     elements.nodes.forEach(node => {
-      const width = 0.7;
-      const height = 0.25;
-      const geometry = new THREE.PlaneGeometry(width, height);
-      const material = new THREE.MeshBasicMaterial({ color: getColorByLevel(node.level), side: THREE.DoubleSide });
+      const width = 0.15;
+      const height = 0.9;
+      const shape = new THREE.Shape();
+      shape.moveTo(0, 0);
+      shape.lineTo(0, width);
+      shape.lineTo(height, width);
+      shape.lineTo(height, 0);
+      shape.lineTo(0, 0);
+
+      const extrudeSettings = {
+        steps: 2,
+        depth: 0.2,
+        bevelEnabled: true,
+        bevelThickness: 0.2,
+        bevelSize: 0.25,
+        bevelOffset: 0.5,
+        bevelSegments: 2
+      };
+      const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
+      const material = new THREE.MeshStandardMaterial({
+        color: getColorByLevel(node.level),
+        roughness: 0,
+        metalness: 1,
+        depthTest: true,
+        depthWrite: true,
+        visible: true,
+        side: THREE.FrontSide
+      });
       const rectangle = new THREE.Mesh(geometry, material);
       rectangle.position.set(node.x, node.y, node.z);
       scene.add(rectangle);
@@ -55,7 +95,7 @@ const Graph = () => {
         const textGeometry = new TextGeometry(node.label, {
           font: font,
           size: 0.05,
-          height: 0.002,
+          depth: 0.002,
           curveSegments: 12,
           bevelEnabled: false
         });
